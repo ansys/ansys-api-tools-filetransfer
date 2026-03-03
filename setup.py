01f87b648eb76e1257048b7718f220701a0cd009
Dominik Gresch <greschd@users.noreply.github.com>
2026-03-03 12:08:43 +0100

    Dependency updates (#48)
    Breaking changes:
    - Bump the minimum Python version to 3.10
    - Bump the minimum protobuf version to 5.29
    
    Other functional changes:
    - Bump the minimum version for ansys-tools-protoc-helper to 0.7.0
    - Drop the upper bound for the protobuf version
    
    Non-functional (dev only) changes:
    - Update the main Python version in CI to 3.14
    - Update pre-commit to 4.5.1
    - Update pre-commit hooks

--- setup.py
+++ setup.py
@@ -35,8 +35,8 @@ if __name__ == "__main__":
         long_description_content_type="text/markdown",
         url=f"https://github.com/ansys/{package_name}",
         license="MIT",
-        python_requires=">=3.7",
-        install_requires=["grpcio~=1.17", "protobuf>=3.19,<7"],
+        python_requires=">=3.10",
+        install_requires=["grpcio~=1.17", "protobuf>=5.29"],
         package_dir={"": "src"},
         packages=setuptools.find_namespace_packages("src", include=("ansys.*",)),
         package_data={
